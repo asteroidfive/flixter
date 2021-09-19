b7a547eef14352e59ca899eeb6a00b96c00af49e
asteroidfive <skyytles99@gmail.com>
2021-09-19 18:37:40 -0400

    flix v2

---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Sky Brooks on 9/19/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
